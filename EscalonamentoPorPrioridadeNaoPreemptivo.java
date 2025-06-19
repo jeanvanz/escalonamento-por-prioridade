acce1b3977d9ea9a349239284b131d5e1b0052bb
Gabriel Orsatto <gabrielorsatto@gmail.com>
2025-06-18 21:04:40 -0300

    Ajuste

--- EscalonamentoPorPrioridadeNaoPreemptivo.java
+++ EscalonamentoPorPrioridadeNaoPreemptivo.java
@@ -3,13 +3,13 @@ import java.util.Comparator;
 import java.util.List;
 
 // Classe que representa um processo
-class Processo {
-    String nome;      
+class ProcessoNaoPreemptivo {
+    String nome;
     int tempoExecucao;
     int prioridade;
 
     // Construtor da classe Processo
-    public Processo(String nome, int tempoExecucao, int prioridade) {
+    public ProcessoNaoPreemptivo(String nome, int tempoExecucao, int prioridade) {
         this.nome = nome;
         this.tempoExecucao = tempoExecucao;
         this.prioridade = prioridade;
@@ -19,16 +19,16 @@ class Processo {
 public class EscalonamentoPorPrioridadeNaoPreemptivo {
     public static void main(String[] args) {
         // Cria uma lista de processos
-        List<Processo> processos = new ArrayList<>();
+        List<ProcessoNaoPreemptivo> processos = new ArrayList<>();
 
         // Adiciona processos à lista com nome, tempo de execução e prioridade
-        processos.add(new Processo("P1", 5, 2));
-        processos.add(new Processo("P2", 3, 1));
-        processos.add(new Processo("P17", 8, 4));
-        processos.add(new Processo("P6", 6, 3));
-        processos.add(new Processo("P7", 2, 5));
-        processos.add(new Processo("P22", 7, 6));
-        processos.add(new Processo("P8", 1, 1));
+        processos.add(new ProcessoNaoPreemptivo("P1", 5, 2));
+        processos.add(new ProcessoNaoPreemptivo("P2", 3, 1));
+        processos.add(new ProcessoNaoPreemptivo("P17", 8, 4));
+        processos.add(new ProcessoNaoPreemptivo("P6", 6, 3));
+        processos.add(new ProcessoNaoPreemptivo("P7", 2, 5));
+        processos.add(new ProcessoNaoPreemptivo("P22", 7, 6));
+        processos.add(new ProcessoNaoPreemptivo("P8", 1, 1));
 
         // Ordena a lista de processos com base na prioridade (menor número = maior prioridade)
         processos.sort(Comparator.comparingInt(p -> p.prioridade));
@@ -39,7 +39,7 @@ public class EscalonamentoPorPrioridadeNaoPreemptivo {
         int tempoAtual = 0; 
 
         // Percorre a lista de processos e simula a execução
-        for (Processo p : processos) {
+        for (ProcessoNaoPreemptivo p : processos) {
             System.out.println("Executando " + p.nome + 
                                " | Prioridade: " + p.prioridade + 
                                " | Tempo de Execução: " + p.tempoExecucao + 
